1e8a9e9002548df0cc942a6a894a87c514a2d6d1
Alessandra Muñoz <alessandra.munoz02@inacapmail.cl>
2023-04-18 17:31:28 -0400

    Avance

---
+++ src/components/Login/Register.vue
@@ -0,0 +1,53 @@
+<template>
+
+  <form style="margin: 15px" @submit.prevent="submitForm">
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Email</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model = "emailCreate">
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label">Contresaña</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" v-model = "passCreate">
+    </div>
+    <button type="submit" class="btn btn-primary" @click="createAccount(emailCreate,passCreate)">Guardar</button>
+  </form>
+  
+</template>
+
+<script>
+
+import {callApiAxios} from '../../services/axios.ts';
+
+  export default {
+    data() {
+      return {
+        emailCreate : null,
+        passCreate : null
+      };
+    },
+    components: {
+      
+    },
+    mounted() {
+      
+    },
+    methods: {
+      async createAccount(emailCreate,passCreate){
+
+        let responseAxios = await callApiAxios('post','http://localhost:3000/login/create',{
+          'tipo_perfil':1,
+          'email':emailCreate,
+          'clave':passCreate,
+        });
+
+        if(responseAxios.status == '202'){
+          alert("Usuario ya existe")
+        }
+        if(responseAxios.status == '201'){
+          alert("Usuario insertado");
+        }
+
+      }
+    },
+  };
+</script>
---
+++ src/services/axios.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export async function callApiAxios(httpMethod:string,url:string,data:{})
+{
+  let json = {
+    httpMethod,
+    url,
+    data
+
+  }
+  try{
+ const axiosResponse = await axios({
+  method: httpMethod,
+  url: url,
+  data:data
+})
+  return axiosResponse
+}catch(error){
+console.log(error)
+throw error
+}
+};
